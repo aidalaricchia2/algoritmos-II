330667ff693dc31eb6750cc283dd30b9b1800a14
aidalaricchia2 <aidalaricchiah2@gmail.com>
2023-03-17 12:10:19 -0300

    insert y delete listos

--- practicas/tp-arboles-avl/code/avltree.py
+++ practicas/tp-arboles-avl/code/avltree.py
@@ -1,4 +1,4 @@
-
+#Aida Laricchia legajo:13251
 class AVLTree:
     root = None
 class AVLNode:
@@ -116,9 +116,10 @@ def insertw(B,newnode, currentnode):
             #cuando ya insertamos tenemos que chequear que el arbol siga siendo AVL
             #tendremos que cheaquer en esa rama el bf de cada uno de sus padre si uno 
             #no se encuentra en el intervalo entonces balanceamos
-            if check_balance_parent(newnode) != None:
+            node_desbalance=check_balance_parent(newnode)
+            if node_desbalance != None:
                 #balancear
-                reBalance(B)
+                rebalanceR(B, node_desbalance)
             return newnode.key
         else:
             return insertw(newnode, currentnode.rightnode)
@@ -128,9 +129,10 @@ def insertw(B,newnode, currentnode):
             newnode.parent=currentnode
             currentnode.leftnode=newnode
             #chequeamos solo subieron en el arbol
-            if check_balance_parent(newnode) != None:
+            node_desbalance=check_balance_parent(newnode)
+            if node_desbalance != None:
                 #balancear
-                reBalance(B)
+                rebalanceR(B, node_desbalance)
 
             return newnode.key
         else:
@@ -157,3 +159,161 @@ def insert(B,element,key):
         B.root=newnode
     else:
         return insertw(B,newnode, currentnode)
+
+#Funcion delete
+def deletew(B,deletingnode):
+    if deletingnode!=None:
+        #Caso 1: Hoja
+        if deletingnode.leftnode==None and deletingnode.rightnode==None:
+            if deletingnode==B.root:
+                B.root=None
+                return deletingnode.key
+            else:
+                padre=deletingnode.parent
+                #Me fijo si el nodo a eliminar es el hijo derecho o izquierdo de su padre
+                if padre.rightnode==deletingnode:
+
+                    padre.rightnode=None
+                #balancear
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+                    return deletingnode.key
+                else:
+                    padre.leftnode=None
+                    #balancear
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+                    return deletingnode.key
+                
+        #Caso 2: Un solo hijo
+        #Si es el hijo izquierdo
+        elif deletingnode.rightnode==None:
+            if deletingnode==B.root:
+                B.root=deletingnode.leftnode
+                return deletingnode.key
+            else:
+                padre=deletingnode.parent
+                #Me fijo si el nodo a eliminar es el hijo derecho o izquierdo de su padre
+                if padre.rightnode==deletingnode:
+                    padre.rightnode=deletingnode.leftnode
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+                    return deletingnode.key
+                else:
+                    padre.leftnode=deletingnode.leftnode
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+                    return deletingnode.key
+        #Si es el hijo derecho            
+        elif deletingnode.leftnode==None:
+            if deletingnode==B.root:
+                B.root=deletingnode.rightnode
+                return deletingnode.key
+            else:
+                padre=deletingnode.parent
+                #Me fijo si el nodo a eliminar es el hijo derecho o izquierdo de su padre
+                if padre.rightnode==deletingnode:
+                    padre.rightnode=deletingnode.rightnode
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+                    return deletingnode.key
+                else:
+                    padre.leftnode=deletingnode.rightnode
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+                    return deletingnode.key
+                           
+        #Caso 3: Dos hijos
+        else:
+            aux1=deletingnode.rightnode
+            aux2=deletingnode.leftnode
+            #Buscamos al mayor de menores
+            mayor=mayordemenores(deletingnode)
+            #En el caso de que el mayor de menores tenga hijos izquierdos y no sea el hijo izquierdo del nodo a eliminar:
+            if mayor.leftnode!=None and mayor!=aux2:
+                menor=menorhijoizq(mayor)
+                #Para evitar crear un ciclo con padres=hijos
+                if menor.parent!=deletingnode.leftnode:
+                    menor.leftnode=aux2
+            elif mayor.leftnode==None and mayor!=aux2:
+                mayor.leftnode=aux2
+            mayor.rightnode=aux1        
+
+            if deletingnode==B.root:
+                B.root=mayor
+            else:
+                padre=deletingnode.parent
+                mayor.parent=padre 
+                if padre.rightnode==deletingnode:
+                    padre.rightnode=mayor
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+                else:
+                    padre.leftnode=mayor
+                    node_desbalance=check_balance_parent(padre)
+                    if node_desbalance != None:
+                        #balancear
+                        rebalanceR(B, node_desbalance)
+
+            return deletingnode.key
+    else:
+        return None
+
+#Funcion que encuentra el nodo mayor entre los hijos menores de un nodo
+def mayordemenores(node):
+    currentnode=node.leftnode
+    while currentnode.rightnode!=None:
+        currentnode=currentnode.rightnode
+    currentnode.parent.rightnode=None
+    return currentnode
+
+#Funcion que encuentra el ultimo hijo menor dentro de una rama
+def menorhijoizq(node):
+    if node.leftnode==None:
+        return node
+    else:
+        return menorhijoizq(node.leftnode)
+    
+
+def delete(B,element):
+    currentnode=B.root
+    deletingnode=searchR(currentnode, element)
+    return deletew(B,deletingnode)
+
+def searchR(currentnode,element):
+    if currentnode!=None:
+        if element==currentnode.value:
+            return currentnode        
+        else:
+            izq= searchR(currentnode.leftnode,element)
+            der= searchR(currentnode.rightnode,element)
+            if izq==None and der==None:
+                return None
+            elif izq==None:
+                return der
+            elif der==None:
+                return izq
+    else:
+        return None
+   
+def search(AVL,element):
+    currentnode=AVL.root
+    result=searchR(currentnode, element)
+    if result!=None:
+        return result.key
+    else:
+        return None
